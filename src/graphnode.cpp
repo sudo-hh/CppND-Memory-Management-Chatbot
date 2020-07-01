fe74bb8340cd3fac58bfce50ef92472802e8986f
Hector <hectora7@gmail.com>
2020-07-01 19:40:31 +0200

    First commit, release candidate 2

---
+++ src/graphnode.cpp
@@ -0,0 +1,63 @@
+#include "graphedge.h"
+#include "graphnode.h"
+#include <iostream>
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    //delete _chatBot; 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot = std::move(chatbot);  // Using move assignment operator
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    std::cout << "Moving _chatBot to newNode..." << std::endl;
+    newNode->MoveChatbotHere(std::move(_chatBot));  // Using move constructor
+    // _chatBot ownership has been moved!
+}
+
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
